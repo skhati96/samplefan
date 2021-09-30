3cb28d591e80f5cf6f8f89cb7d3fd9752333ecfe
skhati96 <sanjiv.hati@gmail.com>
2021-09-30 02:01:42 -0400

    Updated source code

--- src/com/example/fanproj/PullCord.java
+++ src/com/example/fanproj/PullCord.java
@@ -4,27 +4,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class PullCord {
+public class PullCord {
 
 	private static final String REVERSE = "R";
 
 	public static void main(String[] args) {
+		System.out.println(args.length);
 		MyFanPullCord1 cord1 = new MyFanPullCord1();
 		MyFanPullCord2 cord2 = new MyFanPullCord2();
         while (true) {
             System.out.println("Pull cord");
-            String s = getLine();
+            String s = readPullCord();
+            if(s == null || s.isBlank() || s.isEmpty()) {
+                System.out.println("String cannot be parsed, it is null or empty.");
+                continue;
+            }
+
             if (isNumeric(s)) {
             	cord1.pull();
             }
             else if (s.equalsIgnoreCase(REVERSE)) {
             	cord2.pull();
+            } else {
+            	System.out.println("Expected either an integer or a character R");
             }
             
         }
 	}
 	
-    static String getLine() {
+    public static String readPullCord() {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String line = null;
         try {
@@ -35,21 +43,16 @@ class PullCord {
         return line;
     }
     
-    static boolean isNumeric(String string) {
+    public static boolean isNumeric(String str) {
 	
-        System.out.println(String.format("Parsing string: \"%s\"", string));
-    		
-        if(string == null || string.equals("")) {
-            System.out.println("String cannot be parsed, it is null or empty.");
-            return false;
-        }
-        
+        System.out.println(String.format("Parsing string: \"%s\"", str));
+    		        
         try {
-        	Integer.parseInt(string);
+        	Integer.parseInt(str);
             return true;
         } catch (NumberFormatException e) {
             System.out.println("Input String cannot be parsed to Integer.");
+            return false;
         }
-        return false;
     }
 }
